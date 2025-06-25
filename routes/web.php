61c693dfc37e178491f7d34c2d7b0b3eb29b29ae
Snrc97 <onur_soner97@hotmail.com>
2025-06-25 17:28:33 +0300

    bug fix, updates

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,8 @@ use App\Http\Controllers\Web\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('dashboard');
 });
-
-
-
 
 
 Route::group([
@@ -16,7 +13,7 @@ Route::group([
     'middleware' => 'authenticate_session',
     'controller' => DashboardController::class
 ], function() {
-    Route::get('{action}/{params?}', function( Request $request, $action, $params = null) {
+    Route::get('dashboard/{action?}/{params?}', function( Request $request, $action = 'index', $params = null) {
         $controller = new DashboardController();
         return $controller->{$action}($request, $params);
     })

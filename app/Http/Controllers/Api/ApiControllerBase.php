9efa4f6ee54e965a3b7f9caa2c504ea081f2f472
Snrc97 <onur_soner97@hotmail.com>
2025-06-25 12:11:19 +0300

    created Base class for api controllers

--- app/Http/Controllers/Api/ApiControllerBase.php
+++ app/Http/Controllers/Api/ApiControllerBase.php
@@ -2,11 +2,29 @@
 
 namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
+use App\Models\ModelBase;
+use Illuminate\Http\JsonResponse;
 
 class ApiControllerBase extends Controller
 {
+    /**
+     *
+     * @var ModelBase
+     */
+    protected $model;
     public function __construct()
     {
         // $this->middleware('auth:api');
+    }
+
+    public function apiResponse($data, $status = 200, $message = null): JsonResponse
+    {
+        $data = [
+            'data' => $data,
+            'message' => $message,
+            'success' => $status == 200,
+            'status' => $status
+        ];
+        return response()->json($data, $status);
     }
 }

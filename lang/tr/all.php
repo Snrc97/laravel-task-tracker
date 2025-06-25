4b5a8b5473ce966b95bcbe8358df33adfba339d2
Snrc97 <onur_soner97@hotmail.com>
2025-06-25 20:05:14 +0300

    new translations

--- lang/tr/all.php
+++ lang/tr/all.php
@@ -3,8 +3,15 @@ return [
 
     'close' => 'Kapat',
     'save' => 'Kaydet',
+    'create' => 'Oluştur',
     'edit' => 'Düzenle',
     'delete' => 'Sil',
+
+    'name' => 'İsim',
+    'created_at' => 'Oluşturulma Tarihi',
+    'updated_at' => 'Güncellenme Tarihi',
+    'status' => 'Durum',
+
 
     'dashboard' => [
         'title' => 'Kontrol Paneli',

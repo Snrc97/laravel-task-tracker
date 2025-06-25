59b23faf139666e5e4e22fe3edb3c5f247fd54c0
Snrc97 <onur_soner97@hotmail.com>
2025-06-25 21:40:37 +0300

    added validation helper, validation methods and fixed bugs

--- app/Helpers/AppHelper.php
+++ app/Helpers/AppHelper.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 function apiResponse($data = null, $status = 200, $message = null): JsonResponse
 {
@@ -11,4 +12,36 @@ function apiResponse($data = null, $status = 200, $message = null): JsonResponse
         'status' => $status,
     ];
     return response()->json($data, $status);
+}
+
+function withValidation(Request $request, $rules, callable $next): JsonResponse
+{
+    try {
+        $validator = Validator::make($request->all(), $rules);
+        if ($validator->fails()) {
+            $errors = $validator->errors()->toArray();
+            if(count($errors) > 0) {
+                $errors = collect($errors)->values()->map(fn($error) => $error[0])->toArray();
+                $errors = implode(separator: '<br>', array: $errors);
+            }
+            else
+            {
+                $errors = null;
+            }
+
+        }
+        $validatedData = $validator->validate();
+    } catch (\Exception $e) {
+    } finally {
+        if ($errors != null) {
+            return apiResponse(
+                [
+                    'message' => $errors,
+                ],
+                401,
+            );
+        }
+
+        return $next($validatedData);
+    }
 }
--- app/Http/Controllers/Api/AuthenticationController.php
+++ app/Http/Controllers/Api/AuthenticationController.php
@@ -5,58 +5,80 @@ namespace App\Http\Controllers\Api;
 use App\Models\UserModel;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthenticationController extends ApiControllerBase
 {
     public function register(Request $request): JsonResponse
     {
-        $validatedData = $request->validate(UserModel::rules("register"));
+        return withValidation($request, UserModel::rules('register'), function ($validatedData) use ($request) {
 
-        $user = UserModel::create([
-            'name' => $validatedData['name'],
-            'email' => $validatedData['email'],
-            'password' => bcrypt($validatedData['password']),
-        ]);
 
-        $token = $user->createToken('auth_token')->plainTextToken;
+            $user = UserModel::create([
+                'name' => $validatedData['name'],
+                'email' => $validatedData['email'],
+                'password' => bcrypt($validatedData['password']),
+            ]);
 
-        return apiResponse([
-            'user' => $user,
-            'token' => $token
-        ], 201);
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return apiResponse(
+                [
+                    'user' => $user,
+                    'token' => $token,
+                ],
+                201,
+            );
+        });
+
     }
 
     public function login(Request $request): JsonResponse
     {
-        $validatedData = $request->validate(UserModel::rules("login"));
+        return withValidation($request, UserModel::rules('login'), function ($validatedData) use ($request) {
+            if (!auth()->attempt($validatedData)) {
+                return apiResponse(
+                    [
+                        'message' => __('auth.failed'),
+                    ],
+                    401,
+                );
+            }
 
-        if (!auth()->attempt($validatedData)) {
-            return apiResponse([
-                'message' => __('auth.failed')
-            ], 401);
-        };
+            $userCheck = UserModel::where(['email' => $validatedData['email'], 'password' => $validatedData['password']])->first();
 
-        $userCheck = UserModel::where(['email' => $validatedData['email'], 'password' => $validatedData['password']])->first();
+            if (!$userCheck) {
+                return apiResponse(
+                    [
+                        'message' => __('auth.failed', ['reason' => __('auth.invalid_credentials')]),
+                    ],
+                    401,
+                );
+            }
 
-        if (!$userCheck) {
-            return apiResponse([
-                'message' => __('auth.failed', ['reason' => __('auth.invalid_credentials')])
-            ], 401);
-        }
+            $user = auth()->user();
+            $token = $user->createToken('auth_token')->plainTextToken;
+            return apiResponse(
+                [
+                    'user' => $user,
+                    'token' => $token,
+                ],
+                200,
+            );
+        });
 
-        $user = auth()->user();
-        $token = $user->createToken('auth_token')->plainTextToken;
-        return apiResponse([
-            'user' => $user,
-            'token' => $token
-        ], 200);
+
+
     }
 
     public function logout(): JsonResponse
     {
         auth()->logout();
-        return apiResponse([
-            'message' => __('auth.logout_success')
-        ], 200);
+        return apiResponse(
+            [
+                'message' => __('auth.logout_success'),
+            ],
+            200,
+        );
     }
 }

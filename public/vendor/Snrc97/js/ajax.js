aa3344e6124523f1721bb5066dddfd48c0ceee87
Snrc97 <onur_soner97@hotmail.com>
2025-06-25 16:47:03 +0300

    added headers

--- public/vendor/Snrc97/js/ajax.js
+++ public/vendor/Snrc97/js/ajax.js
@@ -8,10 +8,15 @@ function Progress() {
  * @returns
  */
 function AjaxRequest(options) {
+    let csrf = $('meta[name="csrf-token"]').attr('content');
     return $.ajax({
         url: options.url,
         type: options.type || 'GET',
         data: options.data || {},
+        headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem('access_token') ?? '',
+            'X-CSRF-TOKEN': csrf
+        },
         dataType: options.dataType || 'json',
         beforeSend: options.beforeSendCallback ?? null,
         success: options.successCallback ?? null,

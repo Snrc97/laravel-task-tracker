759a0868e70889b8476b97fe16671f0c81495662
Snrc97 <onur_soner97@hotmail.com>
2025-06-25 14:53:34 +0300

    creation and update

---
+++ app/Models/UserModel.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class UserModel extends AuthenticableModelBase
+{
+
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var list<string>
+   */
+    protected $fillable = [
+        'name',
+        'email',
+        'email_verified_at',
+        'password',
+        'remember_token',
+    ];
+
+   /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+       /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    public static function rules($type = "register" | "login"): array
+    {
+        switch($type) {
+            case "register":
+                return [
+                    'name' => 'required|string|max:255',
+                    'email' => 'required|string|email|max:255|unique:users',
+                    'password' => 'required|string|confirmed|min:8',
+                ];
+            case "login":
+                return [
+                    'email' => 'required|string|email',
+                    'password' => 'required|string',
+                ];
+        }
+
+        return [];
+
+    }
+}

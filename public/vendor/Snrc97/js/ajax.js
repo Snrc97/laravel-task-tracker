6dfe44b5a6276d6dfb339c15f94082a17f1206cd
Snrc97 <onur_soner97@hotmail.com>
2025-06-25 21:41:25 +0300

    various bug fixes and updates

--- public/vendor/Snrc97/js/ajax.js
+++ public/vendor/Snrc97/js/ajax.js
@@ -1,5 +1,12 @@
-function Progress() {
-    return ``;
+function ProgressVisibility(visible) {
+    $elm = $('.progress-circle-container');
+    if (visible) {
+        $elm.removeClass("d-none");
+    } else {
+        $elm.addClass("d-none");
+    }
+
+    $elm = null;
 }
 
 /**
@@ -9,6 +16,22 @@ function Progress() {
  */
 function AjaxRequest(options) {
     let csrf = $('meta[name="csrf-token"]').attr('content');
+
+        ProgressVisibility(true);
+
+        if(!options.completeCallback) {
+            options.completeCallback = () => {
+                ProgressVisibility(false);
+            }
+        }
+        else
+        {
+            options.completeCallback = () => {
+                ProgressVisibility(false);
+                options.completeCallback();
+            }
+        }
+
     return $.ajax({
         url: options.url,
         type: options.type || 'GET',

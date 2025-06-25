adbf957e70d28ccb658903cc83ce5079204ee63c
Snrc97 <onur_soner97@hotmail.com>
2025-06-25 13:11:28 +0300

    added necessary cdns

--- resources/views/vendor/Snrc97/layout/main.blade.php
+++ resources/views/vendor/Snrc97/layout/main.blade.php
@@ -10,7 +10,18 @@
     </title>
     <link href="{{ asset('vendor/Snrc97/css/site.css') }}" rel="stylesheet">
 
-    @stack('links')
+    <!-- Bootstrap CSS CDN -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Ajax CDN (included with jQuery) -->
+
+    <!-- DataTables CSS CDN -->
+    <link href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css" rel="stylesheet">
+
+
+
+
+    @stack('styles')
 </head>
 
 <body>
@@ -22,9 +33,25 @@
     </div>
     @yield ('footer')
 
+    <!-- jQuery CDN -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+
+    <!-- Bootstrap JS CDN -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+
+    <!-- DataTables JS CDN -->
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
+
+    <script src="{{ asset('vendor/Snrc97/js/ajax.js') }}"></script>
+
+    <script src="{{ asset('vendor/Snrc97/js/datatable.js') }}"></script>
+
+
     <script src="{{ asset('vendor/Snrc97/js/helper.js') }}"></script>
 
+
     @stack('scripts')
+
 
 </body>
 

21e8cebd8d8e80b165dcbcb7eae7af62d9dfae3c
Snrc97 <onur_soner97@hotmail.com>
2025-06-25 16:03:20 +0300

    created routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,29 @@
 <?php
 
+use App\Http\Controllers\Api\AuthenticationController;
+use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group([
+    'prefix' => 'auth',
+    'controller' => AuthenticationController::class,
+], function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+});
+
+Route::group([
+    'prefix' => 'dashboard',
+    'middleware' => 'auth_token',
+], function () {
+    Route::resource('projects', ProjectController::class);
+    Route::resource('tasks', TaskController::class);
+});
+
+
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\Web\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group([
+    'prefix' => 'web',
+    'middleware' => 'authenticate_session',
+    'controller' => DashboardController::class
+], function() {
+    Route::get('{action}', function( Request $request,$action) {
+        $controller = new DashboardController();
+        return $controller->{$action}($request);
+    });
 });

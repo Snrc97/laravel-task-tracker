164f9b40e4fc5bc7909952210a0681da434690db
Snrc97 <onur_soner97@hotmail.com>
2025-06-26 01:49:29 +0300

    bug fix and updates

--- public/vendor/Snrc97/js/ajax.js
+++ public/vendor/Snrc97/js/ajax.js
@@ -1,5 +1,5 @@
 function ProgressVisibility(visible) {
-    $elm = $('.progress-circle-container');
+    $elm = $(".progress-circle-container");
     if (visible) {
         $elm.removeClass("d-none");
     } else {
@@ -15,35 +15,76 @@ function ProgressVisibility(visible) {
  * @returns
  */
 function AjaxRequest(options) {
-    let csrf = $('meta[name="csrf-token"]').attr('content');
+    let csrf = $('meta[name="csrf-token"]').attr("content");
 
-        ProgressVisibility(true);
+    ProgressVisibility(true);
 
-        if(!options.completeCallback) {
-            options.completeCallback = () => {
-                ProgressVisibility(false);
+    let file_upload = false;
+    if (options.file_upload) {
+        file_upload = options.file_upload;
+        delete options.file_upload;
+    }
+
+    if (!options.completeCallback) {
+        options.completeCallback = () => {
+            ProgressVisibility(false);
+        };
+    } else {
+        options.completeCallback = () => {
+            ProgressVisibility(false);
+            options.completeCallback();
+        };
+    }
+
+    switch (options.type) {
+        case "POST":
+            options = {
+                ...options,
+                processData: false,
+                contentType: "application/json",
+            };
+
+            if (!file_upload) {
+                options.data = JSON.stringify(options.data);
+            } else {
+                options.contentType = false;
+                options.data = { ...options.data, _token: csrf };
             }
-        }
-        else
-        {
-            options.completeCallback = () => {
-                ProgressVisibility(false);
-                options.completeCallback();
-            }
-        }
+
+            break;
+        case "GET":
+            break;
+        case "PUT":
+            // delete options.cache;
+            options = {
+                ...options,
+                processData: false,
+                contentType: "application/json",
+            };
+
+            options.data = JSON.stringify(options.data);
+
+            break;
+        case "DELETE":
+            delete options.data;
+            break;
+    }
+
+    // console.log("options", options);
 
     return $.ajax({
         url: options.url,
-        type: options.type || 'GET',
+        type: options.type || "GET",
         data: options.data || {},
         headers: {
-            'Authorization': 'Bearer ' + localStorage.getItem('access_token') ?? '',
-            'X-CSRF-TOKEN': csrf
+            Authorization:
+                "Bearer " + localStorage.getItem("access_token") ?? "",
+            "X-CSRF-TOKEN": csrf,
         },
-        dataType: options.dataType || 'json',
+        dataType: options.dataType || "json",
         beforeSend: options.beforeSendCallback ?? null,
         success: options.successCallback ?? null,
         error: options.errorCallback ?? null,
-        complete: options.completeCallback ?? null
+        complete: options.completeCallback ?? null,
     });
 }

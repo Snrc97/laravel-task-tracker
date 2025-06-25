c8779dcf2288d2ef167abc80647dde73cecc653f
Snrc97 <onur_soner97@hotmail.com>
2025-06-25 12:45:35 +0300

    added dependency injection binding between controller base class and repository interface

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,6 +11,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+
+        $this->app->bind(
+            'App\Repositories\RepositoryInterface',
+            'App\Controllers\ApiControllerBase'
+        );
         //
     }
 

6ef976faf30ce45e7e15f4487bd9a2114840923f
Snrc97 <onur_soner97@hotmail.com>
2025-06-27 01:49:04 +0300

    bug fixes, updates

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web([StartSession::class,
+        $middleware->append([StartSession::class,
     ]);
         // $middleware->api([EnsureFrontendRequestsAreStateful::class, SubstituteBindings::class]);
     })

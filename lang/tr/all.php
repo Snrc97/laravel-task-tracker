b206559e73335898ee6d9776ceedd059c9d77031
Snrc97 <onur_soner97@hotmail.com>
2025-06-27 01:47:48 +0300

    new translations

--- lang/tr/all.php
+++ lang/tr/all.php
@@ -6,20 +6,31 @@ return [
     'create' => 'Oluştur',
     'edit' => 'Düzenle',
     'delete' => 'Sil',
+    'add' => 'Ekle',
+
+    'success' => 'Başarılı',
+    'error' => 'Hata',
+    'unknown_error' => 'Bilinmeyen Hata',
 
     'name' => 'İsim',
     'created_at' => 'Oluşturulma Tarihi',
     'updated_at' => 'Güncellenme Tarihi',
     'status' => 'Durum',
 
+    'users' => [
+        'title' => 'Kullanıcılar',
+        'singular' => 'Kullanıcı',
+    ],
 
     'dashboard' => [
         'title' => 'Kontrol Paneli',
     ],
     'projects' => [
         'title' => 'Projeler',
+        'singular' => 'Proje',
     ],
     'tasks' => [
         'title' => 'Görevler',
+        'singular' => 'Görev',
     ]
 ];

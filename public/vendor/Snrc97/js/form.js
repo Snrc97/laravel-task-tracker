eed1e537ead8cf8227ded9be21531672a577225d
Snrc97 <onur_soner97@hotmail.com>
2025-06-27 17:07:14 +0300

    added reusable helper methods to open modal and fill these

--- public/vendor/Snrc97/js/form.js
+++ public/vendor/Snrc97/js/form.js
@@ -1,3 +1,100 @@
+function FillFormInputs({ mode, container, data }) {
+    $.each(
+        data,
+        function (key, value) {
+
+            if(key == "json_props")
+            {
+
+                let sub_data = [value];
+
+                FillFormInputs({ mode, container, data: sub_data });
+
+                return;
+            }
+
+            console.log(key,data);
+            const input = $(container).find(`input[name="${key}"]`);
+            const input_select = $(container).find(`select[name="${key}"]`);
+            const input_bladewind_select = $(container).find(`.bw-select input[name="${key}"]`);
+
+            const input_textarea = $(container).find(`textarea[name="${key}"]`);
+
+            if ($(input_textarea).length > 0) {
+                $(input_textarea).val(value);
+
+
+            } else if ($(input_select).length > 0) {
+                input_select.val(value).trigger("change");
+            } else if ($(input_bladewind_select).length > 0) {
+
+                input_bladewind_select.val(value).trigger("change");
+                const input_bladewind_select_element = eval("bw_"+key);
+
+
+                if(input_bladewind_select_element.totalItems() > 0)
+                {
+                    input_bladewind_select_element.selectByValue(value);
+                }
+            }
+            else if (input.is(":checkbox")) {
+                const valMap = value == 1 || value == true ? true : false;
+                input.prop("checked", valMap);
+            } else if (input.is(":radio")) {
+                input.filter(`[value="${value}"]`).prop("checked", true);
+            } else {
+                let input_type = $(input).prop("type");
+                if(input_type == "date")
+                {
+                    value = moment(value).format("YYYY-MM-DD");
+                }
+                input.val(value);
+            }
+
+            input.prop("disabled", false);
+            input_select.prop("disabled", false);
+            $(container).find(".modal-footer").prop("hidden", false);
+
+            if (mode == "detail") {
+                input.prop("disabled", true);
+                input_select.prop("disabled", true);
+                $(container).find(".modal-footer").prop("hidden", true);
+            }
+        },
+        [mode, data]
+    );
+}
+
+async function FillSelectOptions({ type, selectElement, url, options }) {
+    if (!options && url) {
+        options = await AjaxGet({ url, notify: "none" }).then((x) => x.data);
+    }
+    if (options) {
+        if(type == "select")
+        {
+
+            console.log(options);
+                // Use the provided options array to fill the select element
+                options.forEach((option) => {
+                    $(selectElement).append(
+                        $("<option>", { value: option.id ?? option.value, text: option.name ?? option.text })
+                    );
+                });
+
+        }
+
+        else if(type == "bladewind-select")
+        {
+            options = options.map(x => {
+                return {
+                    value: x.id ?? x.value,
+                    label: x.name ?? x.text
+                }
+            });
+            populateBladewindSelect(options, selectElement);
+        }
+    }
+}
 
 async function handleLogoutFormSubmit(e) {
     e.preventDefault();
@@ -33,3 +130,4 @@ async function handleSubmit(e,url) {
     }
     await AjaxRequest(options);
 }
+
--- public/vendor/Snrc97/js/modal.js
+++ public/vendor/Snrc97/js/modal.js
@@ -5,8 +5,24 @@ function ClearContainerInput(container) {
     $(container).find('textarea').val('');
 }
 
-function DisplayModal(modalId, display = true) {
+function DisplayModal(modalId, callback = null, display = true) {
     $modal = $('#' + modalId);
     ClearContainerInput($modal);
+    if(callback)
+    {
+        callback($modal);
+    }
     $modal.modal(display ? 'show' : 'hide');
+}
+function CreateModal(modalId) {
+    DisplayModal(modalId, null, true);
+}
+
+function EditModal(modalId, id) {
+    DisplayModal(modalId, (modal)=> {
+        $modal = $(modal);
+        $modal.find('#id').val(id);
+        console.log($modal);
+        FillFormInputs({ mode: 'edit', container: $modal, data: { id } });
+    }, true);
 }
